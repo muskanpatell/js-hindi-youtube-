c107e9fd3b66f982461bb241f5878a858bdf2f36
muskanpatell <muskanpatel66500@gmail.com>
2025-03-10 12:38:21 +0000

    01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,4 +33,29 @@ function logingUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 // console.log(logingUserMessage("Muskan"))
-console.log(logingUserMessage("muskan"))
+// console.log(logingUserMessage("muskan"))
+
+function calculateCardPrice(...num1){
+    return num1
+}
+// console.log(calculateCardPrice(200, 400, 500, 2000));
+
+const user = {
+    UserName: "muskan",
+    price: 199
+}
+function handleObject (anyobject){
+console.log(`UserName is ${anyobject.UserName} and price is ${anyobject.price}`)
+}
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[0]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
